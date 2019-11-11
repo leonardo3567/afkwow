48632dc14312471c42c85ad7ddb535f057eb66b2
leonardo3567 <leonardo35671@hotmail.com>
2019-11-11 17:27:11 -0200

    Update afk.py
    Adicionando o codigo principal

---
+++ afk.py
@@ -0,0 +1,62 @@
+from pynput.keyboard import Key, Controller
+from win32 import win32gui
+
+import time
+from random import randrange
+import random
+
+
+def intervalo():
+    interval = (random.randint(10,20))
+    print(interval)
+    return interval
+
+
+def antiafk():
+    
+    whnd = win32gui.FindWindow( None, 'World of Warcraft',)
+    print(whnd)
+    time.sleep(2)
+    win32gui.SetForegroundWindow(whnd)
+    win32gui.SetActiveWindow(whnd)
+
+    keyboard = Controller()
+
+    keyboard.press(Key.esc)
+    keyboard.release(Key.esc)
+    keyboard.press(Key.space)
+    keyboard.release(Key.space)
+    time.sleep(1)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(1)
+    keyboard.press('/')
+    keyboard.release('/')
+    time.sleep(1)
+    keyboard.press('d')
+    keyboard.release('d')
+    time.sleep(1)
+    keyboard.press('a')
+    keyboard.release('a')
+    time.sleep(1)
+    keyboard.press('n')
+    keyboard.release('n')
+    time.sleep(1)
+    keyboard.press('c')
+    keyboard.release('c')
+    time.sleep(1)
+    keyboard.press('e')
+    keyboard.release('e')
+    time.sleep(1)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+
+while True:
+    antiafk()
+    numero = intervalo()
+    
+    time.sleep(numero)  
+    
+    
+
